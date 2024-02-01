ee26e3ce585bcd53233d1b9edf86b140aa32066e
Suraj Karosia <53578095+1998Suraj@users.noreply.github.com>
2024-02-01 13:16:10 +0530

    Complete 2 Assignments

---
+++ test.js
@@ -0,0 +1,15 @@
+// Array and object Destructuring
+
+//Basic variable assignment
+const foo = ["one", "two", "three"];
+
+const [red, yellow, green] = foo;
+console.log(red); // "one"
+console.log(yellow); // "two"
+console.log(green); // "three"
+
+const { a, ...rest } = { a: 1, b: 2, c: 3 };
+console.log(rest); // { b: 2, c: 3 }
+
+const [first, ...rest1] = [1, 2, 3];
+console.log(first); // [2, 3]
